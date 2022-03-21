805a8a664b42deaceb4166422fdda9ad3041a88e
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2022-03-21 22:18:46 +0800

    feat(helper): remove number

--- helper.py
+++ helper.py
@@ -21,3 +21,11 @@ def remove_punctuation(text: str) -> str:
     :return: cleaned text
     """
     return text.translate(str.maketrans('', '', string.punctuation))
+
+
+def remove_number(text: str) -> str:
+    """remove number from the input text
+    :param text: input text
+    :return: cleaned text
+    """
+    return text.translate(str.maketrans('', '', '0123456789'))
