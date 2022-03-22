7bd1b6c544eb9f4977bebcce2e2b8d8ec538393a
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2022-03-22 12:15:05 +0800

    chore(classifier): tune parameter

--- main.py
+++ main.py
@@ -56,20 +56,21 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
 # define the keras model
 model = Sequential()
 model.add(Dense(256, input_dim=618, activation='relu'))
-model.add(Dropout(0.3, input_shape=(512,)))
+model.add(Dropout(0.4))
 model.add(Dense(512,
                 activation='relu',
-                kernel_regularizer=regularizers.l2(1e-3),
-                bias_regularizer=regularizers.l2(1e-3),
-                activity_regularizer=regularizers.l2(1e-3)
+                kernel_regularizer=regularizers.l2(1e-2),
+                bias_regularizer=regularizers.l2(1e-2),
+                activity_regularizer=regularizers.l2(1e-2)
                 ))
-model.add(Dropout(0.4, input_shape=(512,)))
+model.add(Dropout(0.4))
 model.add(Dense(64,
                 activation='relu',
-                kernel_regularizer=regularizers.l2(1e-3),
-                bias_regularizer=regularizers.l2(1e-3),
-                activity_regularizer=regularizers.l2(1e-3)
+                kernel_regularizer=regularizers.l2(1e-2),
+                bias_regularizer=regularizers.l2(1e-2),
+                activity_regularizer=regularizers.l2(1e-2)
                 ))
+model.add(Dropout(0.3))
 model.add(Dense(32, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
 
@@ -78,7 +79,7 @@ model.compile(loss='binary_crossentropy',
               optimizer='adam', metrics=['accuracy'])
 
 # fit the keras model on the dataset
-model.fit(X_train, y_train, epochs=300, batch_size=512, verbose=1)
+model.fit(X_train, y_train, epochs=500, batch_size=512, verbose=1)
 # make class predictions with the model
 probs = model.predict(X_test)
 predictions = ((probs) > 0.5).astype(int)
@@ -90,3 +91,11 @@ fpr, tpr, auc_score = compute_roc_auc(probs, y_test)
 print("Accuracy\t: ", acc)
 print("F1 Score\t: ", f1)
 print("AUC\t\t: ", auc_score)
+
+# Performance of MLP
+#    Acc Train  Acc Test  F1 Train   F1 Test  AUC Train  AUC Test
+# 0   0.998382  0.865036  0.998725  0.894266   0.999975  0.932539
+# 1   0.998119  0.867614  0.998518  0.896563   0.999956  0.935964
+# 2   0.998237  0.869613  0.998612  0.897873   0.999971  0.934790
+# 3   0.998158  0.867614  0.998550  0.896205   0.999962  0.936098
+# 4   0.998198  0.866772  0.998580  0.895225   0.999951  0.935521
