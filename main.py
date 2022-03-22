0e46514c9c6fd56299fc76d90bfc2c19f624bb18
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2022-03-22 11:51:31 +0800

    feat(classifier): implement MLP in keras

---
+++ main.py
@@ -0,0 +1,78 @@
+# first neural network with keras make predictions
+from numpy import loadtxt
+import pandas as pd
+
+import sklearn
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.metrics import roc_curve, auc, accuracy_score, f1_score
+from sklearn.model_selection import train_test_split
+
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras import regularizers
+
+
+import helper
+
+
+def countvect(data):
+    count_vectorizer = CountVectorizer(analyzer='char', ngram_range=(2, 2))
+
+    emb = count_vectorizer.fit_transform(data)
+
+    return emb, count_vectorizer
+
+
+def compute_roc_auc(y_prob, y):
+    fpr, tpr, thresholds = sklearn.metrics.roc_curve(y, y_prob)
+    auc_score = sklearn.metrics.auc(fpr, tpr)
+    return fpr, tpr, auc_score
+
+
+def compute_score(y_pred, y):
+    acc = sklearn.metrics.accuracy_score(y, y_pred)
+    f1 = sklearn.metrics.f1_score(y, y_pred)
+    return acc, f1
+
+
+# load the dataset
+names = pd.read_csv("data/name_gender.csv")
+names["name"] = names["name"].apply(lambda x: x.lower())
+names["name"] = names.apply(lambda row: helper.remove_punctuation(row["name"]), axis=1)
+names["name"] = names.apply(lambda row: helper.remove_number(row["name"]), axis=1)
+
+cv_features, count_vectorizer = countvect(names["name"])
+
+X = pd.DataFrame(data=cv_features.toarray(),
+                 columns=count_vectorizer.get_feature_names())
+
+y = names["gender"].apply(lambda x: 1 if x == "F" else 0)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
+
+
+# define the keras model
+model = Sequential()
+model.add(Dense(64, input_dim=618, activation='relu'))
+
+model.add(Dense(8, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+# compile the keras model
+model.compile(loss='binary_crossentropy',
+              optimizer='adam', metrics=['accuracy'])
+
+# fit the keras model on the dataset
+model.fit(X_train, y_train, epochs=30, batch_size=256, verbose=1)
+# make class predictions with the model
+probs = model.predict(X_test)
+predictions = ((probs) > 0.5).astype(int)
+
+acc, f1 = compute_score(predictions, y_test)
+fpr, tpr, auc_score = compute_roc_auc(probs, y_test)
+
+
+print("Accuracy\t: ", acc)
+print("F1 Score\t: ", f1)
+print("AUC\t\t: ", auc_score)
